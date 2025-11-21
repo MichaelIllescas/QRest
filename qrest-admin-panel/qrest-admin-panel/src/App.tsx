4eb8002055702df25825d7072dcaab167df28ac5
MichaelIllescas <joni.illes@hotmail.com>
2025-11-21 02:21:21 -0300

    feat: initial  project structure setup qrest-admin-panel

---
+++ qrest-admin-panel/qrest-admin-panel/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '3rem',
+      fontWeight: 'bold'
+    }}>
+      QRest
+    </div>
+  )
+}
+
+export default App
